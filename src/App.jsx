4765652cf8569264887cad4c5fc40aaceb50b362
Rag-795 <raghavnagarajan23@gmail.com>
2026-04-05 22:05:28 +0530

    Initial Commit: React + TailwindCSS initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl text-green-600 font-extrabold flex items-center justify-center'>Finance Dashboard UI</h1>
+    </>
+  )
+}
+
+export default App
